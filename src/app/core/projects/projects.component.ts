c85c7824cfd6d4e5d7d1cb2faf706aedbe939b4e
KerimBoyukakPXL <93250096+KerimBoyukakPXL@users.noreply.github.com>
2025-04-02 15:36:52 +0200

    update

--- src/app/core/projects/projects.component.ts
+++ src/app/core/projects/projects.component.ts
@@ -13,12 +13,12 @@ export class ProjectsComponent {
   projects = [
     {
       title: 'IT-project - DataSense',
-      description: 'An HR-tool build with Java, Angular, Snowflake, Python.',
+      description: 'An HR-tool build with Java, Angular, Snowflake and Python.',
       link: 'https://example.com/project1'
     },
     {
       title: 'Internship - Fenego',
-      description: 'An Angular PWA for sick children.',
+      description: 'An Angular PWA developed for sick children.',
       link: 'https://example.com/project2'
     }
   ];
